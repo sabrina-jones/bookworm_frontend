77ec8932b2de7baa1271fc9bd53287bb5d9eaa23
sabrina-jones <sabrina.jones87@gmail.com>
2021-06-15 06:42:16 -0500

    post

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const endPoint = "http://localhost:3000/api/v1/books"
 
 document.addEventListener('DOMContentLoaded', () =>{ 
+    //fetch and load books
     getBooks()
+
+    // event listner and handler for create book form
+    const createBookForm = document.querySelector("#create-book-form")
+
+    createBookForm.addEventListener("submit", (e) => createFormHandler(e))
    });
    
    function getBooks(){
@@ -9,7 +15,6 @@ document.addEventListener('DOMContentLoaded', () =>{
     .then(response => response.json())
     .then(books => {
       books.data.forEach(book => {
-        // double check how your data is nested in the console so you can successfully access the attributes of each individual object
         const bookMarkup = `
           <div data-id=${book.id}>
             <p>${book.attributes.category.name}</p>
@@ -24,3 +29,43 @@ document.addEventListener('DOMContentLoaded', () =>{
         })
    })
 }
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const authorInput = document.querySelector('#input-author').value
+    const overviewInput = document.querySelector('#input-overview').value
+    const imageInput = document.querySelector('#input-url').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetch(titleInput, authorInput, overviewInput, imageInput, categoryId)
+  }
+
+  function postFetch(title, author, overview, image_url, category_id) {
+    fetch(endPoint, {
+      // POST request
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        title: title, 
+        author: author, 
+        overview: overview, 
+        image_url: image_url, 
+        category_id: category_id,
+    })
+  })
+    .then(response => response.json())
+    .then(book => {
+      const bookData = book.data.attributes
+      // render JSON response
+      const bookMarkup = `
+      <div data-id=${book.id}>
+        <img src=${bookData.image_url} height="200" width="250">
+        <h3>${bookData.title}</h3>
+        <p>${bookData.category.name}</p>
+        <button data-id=${bookData.id}>edit</button>
+      </div>
+      <br><br>`;
+
+    document.querySelector('#book-container').innerHTML += bookMarkup;
+    })
+  }
